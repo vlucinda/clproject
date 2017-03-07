b0293711161170240c9d96c5ae9fa182849cc95c
vlucinda <vlucinda@yahoo.com>
2017-03-06 20:11:56 -0500

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      
+  });
+});
